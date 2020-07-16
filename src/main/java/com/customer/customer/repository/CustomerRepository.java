20005f15d39c4ae2836183bf55e49b41d587e077
dmendozy <dmendozy@everis.com>
2020-07-16 12:32:35 -0500

    Customer Repository Update

--- src/main/java/com/customer/customer/repository/CustomerRepository.java
+++ src/main/java/com/customer/customer/repository/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends ReactiveMongoRepository<Customer,String> {
+public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
 }
