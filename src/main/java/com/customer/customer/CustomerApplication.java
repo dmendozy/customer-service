3d3152a9aa33d5af2978613de07deebea7ddce33
dmendozy <dmendozy@everis.com>
2020-07-17 15:44:49 -0500

    Update Config

--- src/main/java/com/customer/customer/CustomerApplication.java
+++ src/main/java/com/customer/customer/CustomerApplication.java
@@ -2,12 +2,12 @@ package com.customer.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @EnableSwagger2WebFlux
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerApplication {
 
 	public static void main(String[] args) {
