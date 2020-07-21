41b93596eae1a419eef69afa98b28ba9f3537691
dmendozy <dmendozy@everis.com>
2020-07-21 16:28:54 -0500

    update

--- src/main/java/com/customer/customer/service/CustomerServiceImp.java
+++ src/main/java/com/customer/customer/service/CustomerServiceImp.java
@@ -1,15 +1,12 @@
 package com.customer.customer.service;
 
-import com.customer.customer.adds.Account;
 import com.customer.customer.client.AccountClient;
 import com.customer.customer.client.CreditClient;
 import com.customer.customer.client.TransactionClient;
-import com.customer.customer.model.Customer;
 import com.customer.customer.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
 
 @Service
 public class CustomerServiceImp {
@@ -23,11 +20,4 @@ public class CustomerServiceImp {
     @Autowired
     TransactionClient transactionClient;
 
-    /*public Customer getCustomerAccounts(String id){
-        Mono<Customer> customer= customerRepository.findById(id);
-        if (null != customer){
-            Flux<Account> account = accountClient.getAccountById(customer.flatMap(c->c.getCustomerId().equals(id)instanceof Mono).toString());
-
-        }
-    }*/
 }
