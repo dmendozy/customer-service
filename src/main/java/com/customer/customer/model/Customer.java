086272c4ca04b09dee6397512c2fb4c3d0cfbecb
dmendozy <dmendozy@everis.com>
2020-07-21 16:28:43 -0500

    add constructor

--- src/main/java/com/customer/customer/model/Customer.java
+++ src/main/java/com/customer/customer/model/Customer.java
@@ -3,14 +3,14 @@ package com.customer.customer.model;
 import com.customer.customer.adds.Account;
 import com.customer.customer.adds.Credit;
 import com.customer.customer.adds.Transaction;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @Document(collection = "customers")
@@ -26,10 +26,18 @@ public class Customer {
         super();
     }
 
+    public Customer(String customerId, String email, String phone, String address) {
+        this.customerId = customerId;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
     @Transient
     public Account account;
     @Transient
     public Credit credit;
     @Transient
     public Transaction transaction;
+
 }
