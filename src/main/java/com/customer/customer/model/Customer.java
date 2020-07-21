d3ed226b15c9c0b78afb3a49bfc1b3112d284c62
dmendozy <dmendozy@everis.com>
2020-07-21 14:53:08 -0500

    update

--- src/main/java/com/customer/customer/model/Customer.java
+++ src/main/java/com/customer/customer/model/Customer.java
@@ -1,7 +1,11 @@
 package com.customer.customer.model;
 
+import com.customer.customer.adds.Account;
+import com.customer.customer.adds.Credit;
+import com.customer.customer.adds.Transaction;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -21,4 +25,11 @@ public class Customer {
     public Customer(){
         super();
     }
+
+    @Transient
+    public Account account;
+    @Transient
+    public Credit credit;
+    @Transient
+    public Transaction transaction;
 }
