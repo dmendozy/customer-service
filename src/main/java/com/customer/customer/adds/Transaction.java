3c1f8e011086d35810217310d39dc5a307d09d52
dmendozy <dmendozy@everis.com>
2020-08-03 18:25:24 -0500

    update

--- src/main/java/com/customer/customer/adds/Transaction.java
+++ src/main/java/com/customer/customer/adds/Transaction.java
@@ -2,12 +2,15 @@ package com.customer.customer.adds;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Transaction {
     public String transactionId;
     public String transactionName;
-    public double output;
-    public double input;
-    public String datetime;
+    public double amount;
+    public double commission;
+    public LocalDate datetime;
     public String accountId;
+    public String creditId;
 }
